e652da0d9154a5779e637d016d2ceb16a25f0625
Yasmin Ledesma <yaslesd@gmail.com>
2024-01-23 21:03:22 -0300

    fix(test): momentarily remove handler tests

--- main.go
+++ main.go
@@ -5,19 +5,20 @@ import (
 	"log"
 	"net/http"
 
-    "github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
 	router := httprouter.New()
 
-    router.GET("/api/posts/", GetPosts)
-    router.POST("/api/posts/", AddPost)
-    router.GET("/api/posts/:pid/", GetPost)
-    router.DELETE("/api/posts/:pid/", DeletePost)
-    router.POST("/api/posts/:pid/comments/", AddComment)
-    router.GET("/api/posts/:pid/comments/", GetComments)
-    router.POST("/api/posts/:pid/comments/:cid", UpdateComment)
+	// TODO: make an api middleware
+	router.GET("/api/posts/", GetPosts)
+	router.POST("/api/posts/", AddPost)
+	router.GET("/api/posts/:pid/", GetPost)
+	router.DELETE("/api/posts/:pid/", DeletePost)
+	router.POST("/api/posts/:pid/comments/", AddComment)
+	router.GET("/api/posts/:pid/comments/", GetComments)
+	router.POST("/api/posts/:pid/comments/:cid", UpdateComment)
 
 	fmt.Println("Server running on http://localhost:8080...")
 	log.Fatal(http.ListenAndServe(":8080", router))
